4bae73b44467c88843e78178eb08ffe1fc61bae3
kharioki <antolisah@gmail.com>
2020-08-04 22:26:54 +0300

    added pagination to fetchPeople

--- src/components/People.js
+++ src/components/People.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import React, { useState } from 'react';
+import { usePaginatedQuery } from 'react-query';
 import Person from './Person';
 
-const fetchPeople = async () => {
-  const res = await fetch('https://swapi.dev/api/people/');
+const fetchPeople = async (key, page) => {
+  const res = await fetch(`https://swapi.dev/api/people/?page=${page}`);
   return res.json();
 };
 
 const People = () => {
-  const { data, status } = useQuery('people', fetchPeople);
+  const [page, setPage] = useState(1);
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ['people', page],
+    fetchPeople
+  );
 
   return (
     <div>
@@ -19,11 +23,36 @@ const People = () => {
       {status === 'error' && <div>Error fetching data</div>}
 
       {status === 'success' && (
-        <div>
-          {data.results.map(person => (
-            <Person key={person.name} person={person} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() =>
+              setPage(prevPage =>
+                // if on page 1, it returns page 1
+                Math.max(prevPage - 1, 1)
+              )
+            }
+            disabled={page === 1}
+          >
+            Prev Page
+          </button>
+          <span> {page} </span>
+          <button
+            onClick={() =>
+              setPage(prevPage =>
+                // if we dont have a next page data, we stay on page else we go to next page
+                !latestData || !latestData.next ? prevPage : prevPage + 1
+              )
+            }
+            disabled={!latestData || !latestData.next}
+          >
+            Next Page
+          </button>
+          <div>
+            {resolvedData.results.map(person => (
+              <Person key={person.name} person={person} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
