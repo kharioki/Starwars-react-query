04f2c81891c44563b2b6e4563bccca5dc8e510f8
kharioki <antolisah@gmail.com>
2020-08-04 22:03:55 +0300

    crude pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async (key, greeting) => {
+const fetchPlanets = async (key, greeting, page) => {
   console.log(greeting);
-  const res = await fetch('https://swapi.dev/api/planets/');
+  const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
+  const [page, setPage] = useState(1);
+
   const { data, status } = useQuery(
-    ['planets', 'Hey, Mr Stark'],
+    ['planets', 'Hey, Mr Stark', page],
     fetchPlanets,
     {
       // defualt staletime is 0
       staleTime: 2000,
+      // you can use callback functions
       onSuccess: () => console.log('data fetched')
     }
   );
@@ -22,6 +25,14 @@ const Planets = () => {
   return (
     <div>
       <h2>Planets</h2>
+
+      <button onClick={() => setPage(prevPage => prevPage - 1)}>
+        Prev Page
+      </button>
+      <span> {page} </span>
+      <button onClick={() => setPage(prevPage => prevPage + 1)}>
+        Next Page
+      </button>
 
       {status === 'loading' && <div>Loading data...</div>}
 
