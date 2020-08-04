c5eb9b189691fda4247a296e38cdfe981bbf47e2
kharioki <antolisah@gmail.com>
2020-08-04 20:29:47 +0300

    using useQuery to fetch planets

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
+const fetchPlanets = async () => {
+  const res = await fetch('https://swapi.dev/api/planets/');
+  return res.json();
+};
 const Planets = () => {
+  const { data, status } = useQuery('planets', fetchPlanets);
+
+  console.log({ data });
   return (
     <div>
       <h2>planets</h2>
+      <p>{status}</p>
     </div>
   );
 };
